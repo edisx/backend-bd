b90fa9dfbad5f49b09618ee9d6af01c12f93cc8c
brawlstars15 <eduardas256@gmail.com>
2023-10-17 14:44:11 +0300

    user

--- api/urls/user_urls.py
+++ api/urls/user_urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
     path("profile/update/", views.updateUserProfile, name="user-profile-update"),
     path("", views.getUsers, name="users"),
     
-
-    
+    path("delete/<str:pk>/", views.deleteUser, name="user-delete"),
+    path("update/<str:pk>/", views.updateUser, name="user-update"),
     path("<str:pk>/", views.getUserById, name="user"),
     
 ]
--- api/views/user_views.py
+++ api/views/user_views.py
@@ -24,6 +24,43 @@ class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
 
+@api_view(['POST'])
+def registerUser(request):
+    data = request.data
+    try:
+        user = User.objects.create(
+            first_name=data['name'],
+            username=data['email'],
+            email=data['email'],
+            password=make_password(data['password'])
+        )
+    except:
+        message = {'detail': 'User with this email already exists'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
+    
+    serializer = UserSerializerWithToken(user, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateUserProfile(request):
+    user = request.user
+    serializer = UserSerializerWithToken(user, many=False)
+    
+    data = request.data
+    user.first_name = data['name']
+    user.username = data['email']
+    user.email = data['email']
+    
+    if data['password'] != '':
+        user.password = make_password(data['password'])
+        
+    user.save()
+    
+    return Response(serializer.data)
+
+
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def getUserProfile(request):
@@ -45,38 +82,30 @@ def getUserById(request, pk):
     serializer = UserSerializer(user, many=False)
     return Response(serializer.data)
 
-@api_view(['POST'])
-def registerUser(request):
-    data = request.data
-    try:
-        user = User.objects.create(
-            first_name=data['name'],
-            username=data['email'],
-            email=data['email'],
-            password=make_password(data['password'])
-        )
-    except:
-        message = {'detail': 'User with this email already exists'}
-        return Response(message, status=status.HTTP_400_BAD_REQUEST)
-    
-    serializer = UserSerializerWithToken(user, many=False)
-    return Response(serializer.data)
+@api_view(['DELETE'])
+@permission_classes([IsAdminUser])
+def deleteUser(request, pk):
+    userForDeletion = User.objects.get(id=pk)
+    userForDeletion.delete()
+    return Response('User was deleted')
 
 @api_view(['PUT'])
-@permission_classes([IsAuthenticated])
-def updateUserProfile(request):
-    user = request.user
-    serializer = UserSerializerWithToken(user, many=False)
+@permission_classes([IsAdminUser])
+def updateUser(request, pk):
+    user = User.objects.get(id=pk)
+    serializer = UserSerializer(user, many=False)
     
     data = request.data
-    user.first_name = data['name']
-    user.username = data['email']
-    user.email = data['email']
+    user.first_name = data.get('name', user.first_name)
+    user.username = data.get('email', user.username)
+    user.email = data.get('email', user.email)
+    user.is_staff = data.get('isAdmin', user.is_staff)
     
-    if data['password'] != '':
-        user.password = make_password(data['password'])
-        
     user.save()
     
     return Response(serializer.data)
+
+
+
+
 
